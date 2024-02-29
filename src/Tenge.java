6d4f57cbdd07c0c4b1fb98f633d5bfdef6acd8d7
diaszakir <153705027+diaszakir@users.noreply.github.com>
2024-02-29 13:40:06 +0600

    Update Tenge.java

--- src/Tenge.java
+++ src/Tenge.java
@@ -1,6 +1,29 @@
-public class ExchangeTo implements MoneyExchange{
-    @Override
-    public void exchangeTo(String m1, int cash, String m2) {
+import java.util.Scanner;
 
+public class Tenge implements MoneyExchange{
+    private String currencyTo;
+
+    public Tenge(String currencyTo) {
+        this.currencyTo = currencyTo;
+    }
+
+    @Override
+    public void exchangeTo(Scanner scanner) {
+        System.out.println("Введите сумму в тенге:");
+        double amount = scanner.nextDouble();
+        switch (currencyTo) {
+            case "Доллар":
+                System.out.println("Сумма в долларах: " + amount / 450);
+                break;
+            case "Евро":
+                System.out.println("Сумма в евро: " + amount / 480);
+                break;
+            case "Рубли":
+                System.out.println("Сумма в рублях: " + amount / 5);
+                break;
+            default:
+                System.out.println("Неизвестная валюта");
+                break;
+        }
     }
 }
