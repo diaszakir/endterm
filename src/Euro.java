48a2196ab121efaa527b0165d043daff18af60cc
diaszakir <153705027+diaszakir@users.noreply.github.com>
2024-02-29 13:39:31 +0600

    Euro.java

--- src/Euro.java
+++ src/Euro.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Euro {
+import java.util.Scanner;
+
+public class Euro implements MoneyExchange{
+    private String currencyTo;
+
+    public Euro(String currencyTo) {
+        this.currencyTo = currencyTo;
+    }
+
+    @Override
+    public void exchangeTo(Scanner scanner) {
+        System.out.println("Введите сумму в евро:");
+        double amount = scanner.nextDouble();
+        switch (currencyTo) {
+            case "Тенге":
+                System.out.println("Сумма в тенге: " + amount * 520);
+                break;
+            case "Доллар":
+                System.out.println("Сумма в долларах: " + amount / 0.93);
+                break;
+            case "Рубли":
+                System.out.println("Сумма в рублях: " + amount / 99);
+                break;
+            default:
+                System.out.println("Неизвестная валюта");
+                break;
+        }
+    }
 }
